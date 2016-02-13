918ff320aee1bb8b0e705527ab5742bb9e57e079
Timothy Vernon <timothy.n.vernon@gmail.com>
2016-02-13 08:24:28 -0500

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name asciiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the asciiApp
+ */
+angular.module('asciiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
