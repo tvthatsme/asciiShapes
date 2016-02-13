af27742f33bce1978354464da9a0f38456c83507
Timothy Vernon <timothy.n.vernon@gmail.com>
2016-02-13 09:25:08 -0500

    Added initial controls for height, label, and labelRow

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,18 @@
  * Controller of the asciiApp
  */
 angular.module('asciiApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+  
+    // Choosing the shape is required.
+  
+    // Choosing the height of the shape is required for the user.
+    $scope.shapeHeight = 10;
+  
+    // Choosing the label should be optional for the user.
+    // Default value = 'HI'
+    $scope.shapeLabel = 'HI';
+  
+    // Choosing the label row should be optional for the user.
+    // Default value = 4
+    $scope.labelRow = 4;
   });
